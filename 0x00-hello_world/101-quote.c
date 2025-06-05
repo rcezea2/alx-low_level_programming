bb0e91a7109c0b7bb53ccaaff665246226f7756f
Richard <thekid7590@hotmail.com>
2025-06-05 13:13:32 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -8,7 +7,7 @@
 int main(void)
 {
 	const char *error_message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, error_message, strlen(error_message));
-	return (0);
+	write(2, error_message, 59);
+	return (1);
 }
 
