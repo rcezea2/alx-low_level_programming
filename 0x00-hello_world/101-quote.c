636b493d7951ecdeb376e9b51f7d9ab0c47872dd
Richard <thekid7590@hotmail.com>
2025-06-05 13:39:08 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char *message1 = "and that piece of art is useful\"";
-	char *message2 = " - Dora Korpar, 2015-10-19\n";
+	char *message1 = "and that piece of art is useful";
+	char *message2 = "\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, message1, 31);
 	write(2, message2, 28);
