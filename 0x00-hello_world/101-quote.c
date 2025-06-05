6283ee93886407c054e091bccbbd0c5cd894fd18
Richard <thekid7590@hotmail.com>
2025-06-05 13:34:58 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -7,12 +6,11 @@
  */
 int main(void)
 {
-        char *message1 = "and that piece of art is useful\" ";
-        char *message2 = " - Dora Korpar, 2015-10-19\n";
+	char *message1 = "and that piece of art is useful\"";
+	char *message2 = " - Dora Korpar, 2015-10-19\n";
 
-        write(2, message1, strlen(message1));
-        write(2, message2, strlen(message2));
-
-        return (1);
+	write(2, message1, 31);
+	write(2, message2, 28);
+	return (1);
 }
 
