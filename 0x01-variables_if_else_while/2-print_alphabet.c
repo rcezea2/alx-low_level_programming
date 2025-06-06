ab42ba6d7914be6b617920c217ea771defb66f38
Richard <thekid7590@hotmail.com>
2025-06-06 16:45:31 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
